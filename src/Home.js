75d29ba609bc2c51d1ba8b270c8ca377e762e397
MariusJohn <marius.ciuclea@gmail.com>
2023-01-12 20:41:50 +0000

    added more items on Navbar

--- src/Home.js
+++ src/Home.js
@@ -8,12 +8,13 @@ const Home = () => {
     const [blogs, setBlog] = useState([
         {title: 'My website', body: 'ceva, ceva....', author: 'marius', id: 1},
         {title: 'Welcome !', body: 'ceva, ceva....', author: 'dorina', id: 2},
-        {title: 'Tuition info', body: 'ceva, ceva....', author: 'alex', id: 3}
+        {title: 'Tuition info', body: 'ceva, ceva....', author: 'marius', id: 3}
     ]);
 
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title = "All Blogs !" />
+           <BlogList blogs={blogs.filter((blog)=> blog.author === 'marius')} title = "Marius's blogs" />
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1>My Primary Tutor</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create1">Meet the team</a>
+                <a href="/create2">New Blog</a>
+                <a href="/create3">More</a>
             </div>
         </nav>
         
